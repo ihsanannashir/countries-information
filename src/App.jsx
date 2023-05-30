8cb06c27dd6e677a5a2ea75cdba644d93454056d
ihsanannashir <ihsanannashir@gmail.com>
2023-05-30 22:09:39 +0700

    add main page and mobile details
    main page still need animations, border countries link not yet worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,71 @@
 import './App.css'
 import { Link } from 'react-router-dom'
-// import data from './json/data.json'
-// import React from "react";
-// import Countries from './routes/countries';
+import data from './json/data.json'
+import PropTypes from 'prop-types';
+
+// - See all countries from the API on the homepage
+// - Search for a country using an `input` field
+// - Filter countries by region
+// - Click on a country to see more detailed information on a separate page
+// - Click through to the border countries on the detail page
+// - Toggle the color scheme between light and dark mode *(optional)*
+
+function CountriesCard(props) {
+  return (
+    <div className="rounded-md bg-white dark:bg-dark-element w-[80%] md:w-[250px] mx-4 lg:m-0 shadow-md">
+      <Link to={`./countries/${props.id}`}>
+        <img
+            src={props.image}
+            alt={props.title}
+            className="rounded-t-md object-cover md:h-[167px] md:w-[250px] w-full"
+        />
+        <div className="px-4 pt-4 pb-2 relative">
+          <h2 className="font-bold">{props.title}</h2>
+          <div className='text-sm my-3'>
+            <div><span className='font-semibold'>Population:</span> {props.pop.toLocaleString("en-US")}</div>
+            <div><span className='font-semibold'>Region:</span> {props.region}</div>
+            <div><span className='font-semibold'>Capital:</span> {props.capital}</div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+CountriesCard.propTypes = {
+  id: PropTypes.any,
+  title: PropTypes.string,
+  image: PropTypes.string,
+  pop: PropTypes.number,
+  region: PropTypes.string,
+  capital: PropTypes.string,
+};
+
+CountriesCard.defaultProps = {
+  title: 'No Name',
+  image: "https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80",
+  pop: 'unknown',
+  region: 'N/A',
+  capital: 'N/A',
+};
 
 function App() {
 
   return (
     <>
-      <div>sdkjfhg</div>
-      <Link to="/countries/4">Coba klik ini</Link>
+    <div className='my-6 flex flex-wrap justify-center lg:justify-between gap-y-12'>
+      {data.map((item, idx) => {
+        return <CountriesCard
+          key={idx}
+          id={idx}
+          title={item.name}
+          image={item.flags.png}
+          pop={item.population}
+          region={item.region}
+          capital={item.capital}
+        />
+      })}
+    </div>
     </>
   )
 }
--- src/routes/countries.jsx
+++ src/routes/countries.jsx
@@ -1,10 +1,76 @@
 import data from '../json/data.json'
 import { useParams } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import { BsArrowLeft } from "react-icons/bs";
+import {countries as contParse} from 'country-data';
 
 export default function Countries() {
     let { countriesId } = useParams();
+    const cont = data[countriesId]
+
+    // const contId = data.map((item, idx) => {
+    //   return item.alpha3Code
+    // })
 
   return (
-    <div>Countries {data[countriesId].name}</div>
+    <div className='space-y-10'>
+      <Link to={`/`}>
+        <div className='bg-white dark:bg-dark-element flex flex-row w-28 py-2 shadow-lg rounded-sm mt-8 items-center justify-center space-x-2'>
+          <BsArrowLeft size={18}/>
+          <span className='font-semibold text-sm'>Back</span>
+        </div>
+      </Link>
+
+      <div className='w-full h-[25%] bg-slate-700'>
+        <img
+          src={cont.flags.png}
+          alt={cont.name}
+          className='h-full w-full object-cover'
+        />
+      </div>
+      <div className='space-y-8'>
+        <div className='font-bold text-2xl'>{cont.name}</div>
+
+        <div className='text-sm space-y-2'>
+          <div><span className='font-semibold'>Native Name:</span> {cont.nativeName}</div>
+          <div><span className='font-semibold'>Population:</span> {cont.population.toLocaleString("en-US")}</div>
+          <div><span className='font-semibold'>Region:</span> {cont.region}</div>
+          <div><span className='font-semibold'>Sub Region:</span> {cont.subregion}</div>
+          <div><span className='font-semibold'>Capital:</span> {cont.capital}</div>
+        </div>
+
+        <div className='text-sm space-y-2'>
+          <div><span className='font-semibold'>Top Level Domain:</span> {cont.topLevelDomain}</div>
+          <div>
+            <span className='font-semibold'>Currencies: </span>
+            {cont.currencies.map((item, idx) => {
+              if (idx === cont.currencies.length - 1) {
+                return <span key={idx}>{item.name}</span>
+              }
+              return <span key={idx}>{item.name},</span>
+            })}
+          </div>
+          <div>
+            <span className='font-semibold'>Languages: </span>
+            {cont.languages.map((item, idx) => {
+              if (idx === cont.languages.length - 1) {
+                return <span key={idx}>{item.name}</span>
+              }
+              return <span key={idx}>{item.name}, </span>
+            })}
+          </div>
+        </div>
+        
+        <div className='flex flex-col'>
+          <div className='font-semibold text-lg'>Border Countries:</div>
+          <div className='mt-2 mb-6 flex flex-wrap gap-3'>
+            {cont.borders.map((item, idx) => {
+              return <span key={idx} className='bg-white dark:bg-dark-element px-4 py-2 shadow-md text-sm'>{contParse[item].name}</span>
+            })}
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
